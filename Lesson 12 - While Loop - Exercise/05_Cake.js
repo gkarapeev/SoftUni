1704bf70378d1d0d0c37e2f778ac8cbbbf12debc
Georgi K <45083324+gkarapeev@users.noreply.github.com>
2019-02-05 16:56:01 +0200

    Add files via upload

--- Lesson 12 - While Loop - Exercise/05_Cake.js
+++ Lesson 12 - While Loop - Exercise/05_Cake.js
@@ -1,9 +1,48 @@
 function myFunction(input) {
 
-    let  = parseInt(0);
+    let cakeA = parseInt(input[0]);
+    let cakeB = parseInt(input[1]);
+    let cakeArea = cakeA * cakeB;
+
+
+
+    function notEnoughCake() {
+        return (cakeRemaining - parseInt(userInput)) < 0;
+    }
+
+
+
+    let cakeRemaining = cakeArea;
+    let i = parseInt(2);
+    let userInput = input[i];
+
+
+
+    while (userInput !== "STOP") {
+
+        if (notEnoughCake()) {
+
+            let shortage = Math.abs(cakeRemaining - parseInt(userInput));
+            console.log(`No more cake left! You need ${shortage} pieces more.`);
+            
+            return;
+
+        }
+
+
+        cakeRemaining -= parseInt(userInput);
+
+        i++;
+        userInput = input[i];
+    }
+
+    
+    console.log(`${cakeRemaining} pieces are left.`);
+
 
 }
 
 
 
-myFunction([""]);
+// myFunction(["10", "10", "20", "20", "20", "20", "21"]);
+myFunction(["10", "2", "2", "4", "6", "STOP"]);
